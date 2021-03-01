49a879e7ad7ec153d9468fca4d11a57e040b8e97
Ubaidh Bin Hussain <65657505+SadiqLuther@users.noreply.github.com>
2021-03-01 20:15:38 +0530

    Update 4-master-powershell-script.ps1

--- 4-master-powershell-script.ps1
+++ 4-master-powershell-script.ps1
@@ -1,3 +1,3 @@
 ﻿iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SadiqLuther/windows-startup-scripts/master/1-add-iis-feature.ps1'))
 iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SadiqLuther/windows-startup-scripts/master/2-download-web-app-extract.ps1'))
-iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SadiqLuther/windows-startup-scripts/main/3-allow-http-inbound.ps1'))
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SadiqLuther/windows-startup-scripts/master/3-allow-http-inbound.ps1'))
